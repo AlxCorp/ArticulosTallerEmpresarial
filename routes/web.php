3856b0c1b495fb3a4dbec7b5069a9c62c075421f
Alejandro Priego <alejandro@pfagot.com>
2025-03-11 19:49:26 +0100

    Install Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ArticleController;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-Route::resource('articles', ArticleController::class)
-    ->names('articles');
+require __DIR__.'/auth.php';
