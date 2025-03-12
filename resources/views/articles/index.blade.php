31304ba2b857c05cc7a05e9fddd814bb7579ad81
Alejandro Priego <alejandro@pfagot.com>
2025-03-12 02:09:31 +0100

    Pretty Articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
-    @foreach ($articles as $article)
-    <article>
-        <p>{{$article->title}}</p>
-        @if ($article->img)
-            <img src="{{$article->img}}">
-        @else
-            <img src="https://img.freepik.com/premium-photo/midsection-man-using-laptop-table_1048944-20486397.jpg" width="80px">
-        @endif
-        <a href="{{route('articles.show',$article->slug)}}"><button>Leer Artículo Completo</button></a>
-    </article>
-        {{$article}}
-        <hr>
-    @endforeach
+    <h1 class="text-3xl antialiased font-black text-center my-6">Lee los últimos artículos</h1>
+    <main class="grid grid-cols-3 my-4" style="justify-items: center;">
+        @foreach ($articles as $article)
+        <article>
+            <x-article 
+                slug="{{route('articles.show',$article->slug)}}"
+                img="https://img.freepik.com/premium-photo/midsection-man-using-laptop-table_1048944-20486397.jpg"
+                title="{{$article->title}}"
+                summary="{{Str::limit($article->content, 100)}}"/>
+        </article>
+        @endforeach
+    </main>
 </x-app-layout>
