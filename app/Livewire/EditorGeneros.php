639feb42eb0256d8fdfb9d18fd42715406e390d6
Alejandro Priego <alejandro@pfagot.com>
2025-03-16 21:51:42 +0100

    Create Generos Table

--- app/Livewire/EditorGeneros.php
+++ app/Livewire/EditorGeneros.php
@@ -3,9 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Genre;
 
 class EditorGeneros extends Component
 {
+    public $genres;
+    
+    public function mount()
+    {
+        $this->genres = Genre::all();
+    }
+
     public function render()
     {
         return view('livewire.editor-generos');
