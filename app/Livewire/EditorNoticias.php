989c1d993ff55ecd55a9d734822b2fc89d814b56
Alejandro Priego <alejandro@pfagot.com>
2025-03-16 16:23:57 +0100

    Create noticias table

--- app/Livewire/EditorNoticias.php
+++ app/Livewire/EditorNoticias.php
@@ -3,9 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Article;
 
 class EditorNoticias extends Component
 {
+    public $articles;
+    
+    public function mount()
+    {
+        $this->articles = Article::all();
+    }
+
     public function render()
     {
         return view('livewire.editor-noticias');
