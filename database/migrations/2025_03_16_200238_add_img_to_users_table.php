385189d8735cf9801a15cdfce2e157426a6e4256
Alejandro Priego <alejandro@pfagot.com>
2025-03-16 11:52:20 +0100

    Fix Migration

--- database/migrations/2025_03_16_200238_add_img_to_users_table.php
+++ database/migrations/2025_03_16_200238_add_img_to_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('img')->after('img');
+            $table->longText('img')->after('role');
         });
     }
 
