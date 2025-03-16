01bb9eda48de742e14a2477e078cd1bed3f6d1e7
Alejandro Priego <alejandro@pfagot.com>
2025-03-16 22:16:28 +0100

    Create Lectores Table

--- app/Livewire/EditorLectores.php
+++ app/Livewire/EditorLectores.php
@@ -3,9 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class EditorLectores extends Component
 {
+    public $readers;
+    
+    public function mount()
+    {
+        $this->readers = User::where('role', 'reader')->get();
+    }
+
     public function render()
     {
         return view('livewire.editor-lectores');
--- resources/views/livewire/editor-lectores.blade.php
+++ resources/views/livewire/editor-lectores.blade.php
@@ -1,3 +1,60 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Nombre
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Imagen
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Change Password
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Fecha Creación
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Fecha Edición
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Eliminar
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($readers as $reader)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $reader->name }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $reader->email }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $reader->img }}
+                    </td>
+                    <td class="px-6 py-4">
+                        <button type="button" class="cursor-pointer focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">CAMBIAR CONTRASEÑA</button>
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $reader->created_at }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $reader->updated_at }}
+                    </td>
+                    <td class="px-6 py-4">
+                        <button type="button" class="cursor-pointer focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
