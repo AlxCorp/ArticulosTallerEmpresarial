ec42d589ac2f33550732d3c8759fae2cb2314c48
Alejandro Priego <alejandro@pfagot.com>
2025-03-18 01:45:56 +0100

    Show genres

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -33,6 +33,10 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                 </div>
+                <br>
+                @foreach ($article->genres()->get() as $genre)
+                    <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">{{ $genre->name }}</span>
+                @endforeach
             </div>
         </article>
     @endforeach
