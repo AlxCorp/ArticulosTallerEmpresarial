4d760218f68f706c4c4020c12a42371c7c08da05
Alejandro Priego <alejandro@pfagot.com>
2025-03-18 01:11:28 +0100

    Fix article detail view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
-        <h1>{{$article->title}}</h1>
-        @if ($article->img)
-            <img src="{{$article->img}}">
-        @else
-            <img src="https://img.freepik.com/premium-photo/midsection-man-using-laptop-table_1048944-20486397.jpg" width="80px">
-        @endif
-        <p>{{$article->content}}</p>
-        <form action="{{route('articles.destroy',$article->slug)}}" method="POST">
-            <button type="submit">Eliminar Artículo</button>
-            @csrf
-            @method('delete')
-        </form>
-        {{$article}}
+    <main class="flex flex-col items-center my-4 px-6">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-center">{{$article->title}}</h1>
+        <div class="w-40">
+            @if ($article->img)
+                <img class="h-auto max-w-full" src="/storage/images/{{$article->img}}">
+            @else
+                <img class="h-auto max-w-full" src="https://img.freepik.com/premium-photo/midsection-man-using-laptop-table_1048944-20486397.jpg">
+            @endif
+        </div>
+        <p class="my-2">{{$article->content}}</p>
+    </main>
 </x-app-layout>
